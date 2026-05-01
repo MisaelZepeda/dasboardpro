6777795151c783ea2c5158ca867f5d5d92cb28de
Misael Zepeda <zepedamisael231@gmail.com>
2026-04-30 22:25:06 -0600

    xx

--- js/config/firebase-config.js
+++ js/config/firebase-config.js
@@ -1,17 +1,11 @@
-﻿// Import the functions you need from the SDKs you need
-import { initializeApp } from "firebase/app";
-// TODO: Add SDKs for Firebase products that you want to use
-// https://firebase.google.com/docs/web/setup#available-libraries
-
-// Your web app's Firebase configuration
-const firebaseConfig = {
+﻿export const firebaseConfig = {
   apiKey: "AIzaSyD6EkRkU4eV1tS7dfn5q3EpklTJTMqlSSk",
   authDomain: "dashboard2-63fab.firebaseapp.com",
+  databaseURL: "https://dashboard2-63fab-default-rtdb.firebaseio.com/",
   projectId: "dashboard2-63fab",
   storageBucket: "dashboard2-63fab.firebasestorage.app",
   messagingSenderId: "221672917911",
   appId: "1:221672917911:web:c84703e78d51209792d2d2"
 };
 
-// Initialize Firebase
-const app = initializeApp(firebaseConfig);
+export const hasFirebaseConfig = Object.values(firebaseConfig).every(Boolean);
