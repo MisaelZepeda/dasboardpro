05c85a7a26f4a2c13d9b4acbbd41cdf7f8a6a70c
Misael Zepeda <zepedamisael231@gmail.com>
2026-04-30 22:10:48 -0600

    base de datos

--- js/config/firebase-config.js
+++ js/config/firebase-config.js
@@ -1,11 +1,10 @@
-﻿export const firebaseConfig = {
-  apiKey: '',
-  authDomain: '',
-  databaseURL: '',
-  projectId: '',
-  storageBucket: '',
-  messagingSenderId: '',
-  appId: ''
+﻿const firebaseConfig = {
+  apiKey: "AIzaSyD6EkRkU4eV1tS7dfn5q3EpklTJTMqlSSk",
+  authDomain: "dashboard2-63fab.firebaseapp.com",
+  projectId: "dashboard2-63fab",
+  storageBucket: "dashboard2-63fab.firebasestorage.app",
+  messagingSenderId: "221672917911",
+  appId: "1:221672917911:web:c84703e78d51209792d2d2"
 };
 
 export const hasFirebaseConfig = Object.values(firebaseConfig).every(Boolean);
